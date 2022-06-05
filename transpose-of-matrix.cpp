83b8427411c5a9c586dbc1d9e1c5cac5844560e4
Navya <arora.navya2003@gmail.com>
2022-06-05 15:58:20 +0530

    commit

---
+++ transpose-of-matrix.cpp
@@ -0,0 +1,16 @@
+// ques---https://practice.geeksforgeeks.org/problems/transpose-of-matrix-1587115621/1
+// #include <bits/stdc++.h>
+// class Solution
+// {   
+//     public:  
+//     //Function to find transpose of a matrix.
+//     void transpose(vector<vector<int> >& matrix, int n)
+//     { 
+//         // code here 
+//         for (int i=0;i<n;i++){
+//             for(int j = 0;j<i;j++){
+//                 swap(matrix[i][j], matrix[j][i]);
+//             }
+//         }
+//     }
+// };
