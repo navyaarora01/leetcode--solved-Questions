0e58e158e22e8c0032e9b1e6d98d271ad24d4528
Navya <arora.navya2003@gmail.com>
2022-06-11 20:23:13 +0530

    commit

---
+++ NonRepeatingChar.cpp
@@ -0,0 +1,25 @@
+// ques-https://practice.geeksforgeeks.org/problems/non-repeating-character-1587115620/1
+// #include<bits/stdc++.h>
+// class Solution
+// {
+//     public:
+//     //Function to find the first non-repeating character in a string.
+//     char nonrepeatingCharacter(string s)
+//     {
+        
+//         int n = s.size();
+//         map<char,int> map;
+//         for(int i=0;i<n;i++){
+//             map[s[i]]++;
+//         }
+//         for(int i=0;i<n;i++){
+//             if(map[s[i]]==1){
+//                 return (char)s[i];
+//             }
+//         }
+//         return '$';
+//        //Your code here
+       
+//     }
+
+// };
