4511baadab9c845de4a50752f07deef9e1000893
Navya <arora.navya2003@gmail.com>
2022-06-12 12:54:15 +0530

    commit

---
+++ sortAraayIncFreq.cpp
@@ -0,0 +1,15 @@
+// ques - https://leetcode.com/problems/sort-array-by-increasing-frequency/
+
+// class Solution {
+// public:
+//     vector<int> frequencySort(vector<int>& nums) {
+//         unordered_map<int,int> umap;
+//         for(auto x: nums){
+//             umap[x]++;
+//         }
+//         sort(nums.begin(),nums.end() , [&](int a , int b){
+//             return umap[a]!=umap[b] ? umap[a]<umap[b] : a>b;
+//         });
+//         return nums;
+//     }
+// };
