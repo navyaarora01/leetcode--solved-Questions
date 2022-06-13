bfc0a59ea175134f3ba5786c178ec3d4ca5f5750
Navya <arora.navya2003@gmail.com>
2022-06-13 14:35:29 +0530

    commit

---
+++ postorderusing2stacks.cpp
@@ -0,0 +1,28 @@
+// ques-- https://leetcode.com/problems/binary-tree-postorder-traversal/submissions/ 
+/*class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root == NULL){
+            return ans;
+        }
+        stack<TreeNode *> s1,s2;
+        s1.push(root);
+        while(!s1.empty()){
+            root = s1.top();
+            s1.pop();
+            s2.push(root);
+            if(root->left!=NULL){
+                s1.push(root->left);
+            }
+            if(root->right!=NULL){
+                s1.push(root->right);
+            }
+        }
+        while(!s2.empty()){
+            ans.push_back(s2.top()->val);
+            s2.pop();
+        }
+        return ans;
+    }
+}; */
